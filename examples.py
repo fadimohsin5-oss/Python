2d0a69363f809fd8a28c0985641d8a0bb916d21f
fadimohsin5-oss <fadimohsin5@gmail.com>
2026-05-23 12:15:59 +0100

    ic

---
+++ examples.py
@@ -0,0 +1,14 @@
+name = "Fahad"
+age = 10
+isStudent = True
+marks = 97.67
+
+print(name)
+print(age)
+print(isStudent)
+print(marks)
+
+print("The datatype of name variables is ", type(name))
+print("The datatype of age variables is ", type(age))
+print("The datatype of isStudent variables is ", type(isStudent))
+print("The datatype of marks variables is ", type(marks))
---
+++ operations.py
@@ -0,0 +1,9 @@
+num1 = 16
+num2 = 4
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1//num2)
+print(num1%num2)
+print(num1**num2)
