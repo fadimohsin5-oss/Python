c68a4310acb1b7c7004b78b686e6c65825c2ef8e
fadimohsin5-oss <fadimohsin5@gmail.com>
2026-06-13 12:11:57 +0100

    Turtle

---
+++ triangle.py
@@ -0,0 +1,21 @@
+import turtle
+turtle.Screen().bgcolor("red")
+main= turtle.Screen()
+board=turtle.Turtle()
+
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+
+
---
+++ turtles.py
@@ -0,0 +1,8 @@
+import turtle
+
+turtle.Screen().bgcolor("red")
+t=turtle.Screen()
+t.setup(400,500)
+turtle.title("Welcome")
+
+
