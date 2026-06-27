5df8b396f609f7d2806d8fae5a88c7c13f20fbb1
fadimohsin5-oss <fadimohsin5@gmail.com>
2026-06-27 12:10:27 +0100

    notes

---
+++ loops.py
@@ -0,0 +1,2 @@
+for i in range (1,11):
+    print(f" 17 x {i} = {17*i}")
---
+++ naturalnumbers.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number"))
+sum = 0
+for i in range(0, num+1):
+    sum= sum + i
+print(f"The sum is {sum}")
